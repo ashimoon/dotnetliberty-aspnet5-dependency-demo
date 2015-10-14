39faa40b3705b19cd9a335a9609825a2f47f55d7
Armen Shimoon <armen.shimoon@gmail.com>
2015-10-13 20:56:43 -0700

    Initial commit for demo.

---
+++ src/DependencyDemo/Controllers/GreetingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using DependencyDemo.Services;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DependencyDemo.Controllers
+{
+    public class GreetingController : Controller
+    {
+        private IGreetingService _greetingService;
+        private IGreetingService _secondService;
+
+        public GreetingController(IGreetingService greetingService, IGreetingService secondService)
+        {
+            _greetingService = greetingService;
+            _secondService = secondService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["message"] = _greetingService.GetGreeting() + " " + _secondService.GetGreeting();
+            return View();
+        }
+    }
+}
---
+++ src/DependencyDemo/Services/GreetingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyDemo.Services
+{
+    public class GreetingService : IGreetingService
+    {
+        public string GetGreeting()
+        {
+            return "Hello " + GetHashCode();
+        }
+    }
+}
